e3aec8de7740f21ddda4fba606490a67701196d5
Arif Howlader <ah3288@nyu.edu>
2023-04-29 21:03:20 -0400

    first

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import data from './data.js';
+import Card from './Card.js';
+import Header from './Header.js';
+
+
+
+export default function App() {
+  console.log('above fetch in app');
+
+  const [loading, setLoading] = useState(true);
+  const [ar, setAr] = useState([]);
+
+  useEffect(() => {
+      setLoading(false);
+    setAr(data.map(function (item) {
+      return <Card item={item} />;
+    }));
+
+    
+  }, []);
+
+  if (loading) {
+    return <div className='loading-message'>Loading...............</div>;
+  } else {
+    return (
+      <div className='appdiv'>
+        <Header />
+        {ar}
+      </div>
+    );
+  }
+}
---
+++ Card.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+
+const apiKey = process.env.API_KEY;
+const url = `https://api.openai.com/v1/images/generations`;
+const bearer = 'Bearer ' + apiKey;
+const paramdesc = 'Generate a visually imposing image from this description in the style of Monet ';
+
+export default function Card(props) {
+  const [loading, setLoading] = useState(true);
+  const [link, setLink] = useState('');
+
+  useEffect(() => {
+    callAI(props.item.description);
+  }, []);
+
+  async function callAI(imgdesc) {
+    try {
+        setLoading(true);
+      let newdesc = paramdesc + imgdesc;
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Authorization': bearer,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt: newdesc,
+          n: 1,
+          size: '256x256',
+        }),
+      });
+
+      const data = await res.json();
+      setLink(data.data[0].url);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return <div className='loading-message'>Loading...............</div>;
+  }
+  
+  /* <a href={props.item.googleMapsUrl} target='_blank' className='loc-ref'>
+            View on Google Maps
+          </a> */
+  
+  return (
+    <div className='container'>
+      <div className='main-image-container'>
+        {link && <img src={link} className='main-image' alt='Main' />}
+      </div>
+      <div className='everything'>
+        <div className='loc-block'>
+          <img src='./location-icon.png' className='loc-img' />
+          <span className='location'>{props.item.location}</span>
+          <button className="generate-button" onClick={()=>callAI(props.item.description)}>Regenerate</button>
+
+          
+        </div>
+        <h1 className='loc-title'>{props.item.title}</h1>
+        <p className='loc-date'>
+          {props.item.startDate} - {props.item.endDate}
+        </p>
+        <p className='loc-desc'>{props.item.description}</p>
+      </div>
+    </div>
+  );
+}
